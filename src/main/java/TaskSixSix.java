57c9a736e1ce5bb90e76d0a150b8c4dac6ae2511
Binisha Shrestha <shrestha.binishaa@gmail.com>
2021-01-17 02:25:30 -0600

    Tak 6 complete

--- src/main/java/TaskSixSix.java
+++ src/main/java/TaskSixSix.java
@@ -1,2 +1,19 @@
 public class TaskSixSix {
+
+    public static void main(String[] args) {
+        Programming programming = new Programming();
+        Programming programming1 = new Programming(" dancing");
+    }
+
+}
+class Programming{
+    private String str = " I Love";
+    public Programming() {
+        System.out.println("I love programming languages");
+    }
+
+    public Programming(String str) {
+        this.str += str;
+        System.out.println(this.str);
+    }
 }
