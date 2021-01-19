19e79a2ab4012a36b7fe6b90dcdbe1824f8ca3c1
Binisha Shrestha <shrestha.binishaa@gmail.com>
2021-01-19 01:55:27 -0600

    Task 3 one and two complete

---
+++ src/main/java/TaskThree.java
@@ -0,0 +1,41 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TaskThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number you want the multiplication for");
+        int  num = scanner.nextInt();
+        printMultiplecation(num);
+        System.out.println("Enter the  10 numbers ");
+        int[] arr = new int[10];
+        int i = 0;
+        while (scanner.hasNext()&& i<10){
+            arr[i++] = scanner.nextInt();
+        }
+        printSumof10Numbers(arr);
+
+
+    }
+
+    private static void printSumof10Numbers(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+        }
+        float avg = sum/arr.length;
+        System.out.println(" sum of the array of printed value: "+ sum);
+        System.out.println(" Average of the array of printed value: "+ avg);
+    }
+
+    private static void printMultiplecation(int num) {
+        for (int i = 1; i <=10; i++) {
+            int r = num * i;
+            System.out.println(num +" * "+ i+" = "+ r);
+
+        }
+    }
+
+}
