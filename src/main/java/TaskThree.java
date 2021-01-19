1996fd2c13bcf35c7563ee98f88bfe7b0dd02c66
Binisha Shrestha <shrestha.binishaa@gmail.com>
2021-01-19 03:20:27 -0600

    Task 3 ques 7 complete

--- src/main/java/TaskThree.java
+++ src/main/java/TaskThree.java
@@ -18,9 +18,92 @@ public class TaskThree {
 //        CheckInvalidInput();
 //        PrintSumNaturalNum(20);
 //        longInput();
-        ChecktheinputaescDesc();
+//        ChecktheinputaescDesc();
+        FindDaysinMonth();
 
 
+
+    }
+
+    private static void FindDaysinMonth() {
+        int[] monthDay = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] monthName = {"INVALID","January", " February", "March", "April", "May",
+                        "June", "July", "August", "September", "October",
+                "November", "December"};
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input a month number: ");
+        int month = input.nextInt();
+        System.out.print("Input a year: ");
+        int year = input.nextInt();
+        String monthNametoPrint = "INVALID month number";
+        int monthNumberdays = 0;
+//        if(month != monthDay[1]){
+//            System.out.println(monthName[month] + " "+ year+ " has " + monthDay[month] + " days");
+//        }else{
+//
+//        }
+
+        switch (month){
+            case 1:
+                monthNametoPrint = monthName[1];
+                monthNumberdays = monthDay[1];
+                break;
+            case 2:
+                if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+                    monthNametoPrint = monthName[2];
+                    monthNumberdays = 29;
+                } else {
+                    monthNametoPrint = monthName[2];
+                    monthNumberdays = 28;
+                }
+
+                break;
+            case 3:
+                monthNametoPrint = monthName[3];
+                monthNumberdays = monthDay[3];
+                break;
+            case 4:
+                monthNametoPrint = monthName[4];
+                monthNumberdays = monthDay[4];
+                break;
+            case 5:
+                monthNametoPrint = monthName[5];
+                monthNumberdays = monthDay[5];
+                break;
+            case 6:
+                monthNametoPrint = monthName[6];
+                monthNumberdays = monthDay[6];
+                break;
+            case 7:
+                monthNametoPrint = monthName[7];
+                monthNumberdays = monthDay[7];
+                break;
+            case 8:
+                monthNametoPrint = monthName[8];
+                monthNumberdays = monthDay[8];
+                break;
+            case 9:
+                monthNametoPrint = monthName[9];
+                monthNumberdays = monthDay[9];
+                break;
+            case 10:
+                monthNametoPrint = monthName[10];
+                monthNumberdays = monthDay[10];
+                break;
+            case 11:
+                monthNametoPrint = monthName[11];
+                monthNumberdays = monthDay[11];
+                break;
+            case 12:
+                monthNametoPrint = monthName[12];
+                monthNumberdays = monthDay[12];
+                break;
+
+
+
+
+        }
+        System.out.println(monthNametoPrint + " "+ year+ " has " + monthNumberdays + " days");
 
     }
 
